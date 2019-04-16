a26b6abf881aa9eec4c32e538ba55ed8c85f9a95
xhtdevila <42470168+xhtdevila@users.noreply.github.com>
2019-04-17 00:43:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -24,19 +24,4 @@ let result = str.split('\n\n')
                 .filter(item => checker(item))
                 .join('\r\n')
 
-writeSync(path.resolve(__dirname, BUILD_DIR, 'index.html'), base64(result))
-//2
-let BUILD_DIR = 'dist' // 构建目录
-let ENTRY_FILE = './a.txt' // SS(R)入口
-let str = readSync(ENTRY_FILE)
-
-let checker = item => {
-    return item => item.includes('ssr://') || item.includes('ss"//')
-}
-
-// Map all the item include `ssr://` and serialize those items
-let result = str.split('\n\n')
-                .filter(item => checker(item))
-                .join('\r\n')
-
-writeSync(path.resolve(__dirname, BUILD_DIR, 'xht'), base64(result))
+writeSync(path.resolve(__dirname, BUILD_DIR, 'index1.html'), base64(result))
